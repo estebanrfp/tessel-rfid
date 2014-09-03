26666d9785a713525a3482ab7b64de0094ae9c8a
estebanrfp <estebanrfp@gmail.com>
2014-09-03 21:51:56 +0200

    simplifico código

--- rfid_relay.js
+++ rfid_relay.js
@@ -1,9 +1,7 @@
 var tessel = require('tessel');
 var rfidlib = require('rfid-pn532');
-var relaylib = require('relay-mono');
 
 var rfid = rfidlib.use(tessel.port['A']); 
-var relay = require('relay-mono').use(tessel.port['C']);
 
 var led1 = tessel.led[0].output(1);
 
@@ -12,16 +10,8 @@ rfid.on('ready', function (version) {
 
   rfid.on('data', function(card) {
     
-
-      // if(card.uid.toString('hex') == '6a1f694d')
-      // {
         console.log('UID:', card.uid.toString('hex'));
       	led1.toggle();
-    	  relay.toggle(1);
-    
-      // }
-
-
   });
 });
 
